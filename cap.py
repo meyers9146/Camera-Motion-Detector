26386a75d0c89da64f4688c14010f4b636fe6f2e
Mike Meyers <Mike@The-Mikebook-Pro-2.local>
2019-07-29 15:50:15 -0400

    Camera input and background tracker functional

--- cap.py
+++ cap.py
@@ -8,10 +8,17 @@ Created on Mon Jul 29 12:58:53 2019
 
 import numpy as np
 import cv2 as cv
+
+#cap: the video stream from the local webcam
+#fgbg: the background mask from the local webcam
 cap = cv.VideoCapture(0)
+fgbg = cv.bgsegm.createBackgroundSubtractorGMG()
+
+#If the capture cannot retrieve an image, report an issue with camera
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+    
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -19,13 +26,17 @@ while True:
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Create background mask by subtracting non-moving pixels from frame
+    fgmask = fgbg.apply(frame)
+    
     # Display the resulting frame
-    cv.imshow('frame', gray)
+    cv.imshow('frame', fgmask)
     cv.startWindowThread()
-    if cv.waitKey(1) == ord('q'):
+    
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
         break
+
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
