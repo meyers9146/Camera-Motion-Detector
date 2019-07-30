25dd246a129d93ba7a5afb1e4b9c8b07ccb1c814
Mike Meyers <Mike@The-Mikebook-Pro-2.local>
2019-07-30 12:31:58 -0400

    Seems to be functioning. Cleaned up some test code

--- cap.py
+++ cap.py
@@ -15,24 +15,12 @@ import numpy as np
 import cv2 as cv
 import argparse
 import time
+import datetime
 
 # cap: the video stream from the local webcam
 # fgbg: the background mask from the local webcam
 cap = cv.VideoCapture(0)
 fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
-#time.sleep(10.0) #TODO: let it warm up?
-
-'''
-while True:
-    ret, frame = cap.read()
-    
-    fgmask = fgbg.apply(frame)
-    cv.imshow("window", fgmask)
-    k = cv.waitKey(30) & 0xff
-    if k == 27:
-        break
-'''      
-
 
 # Argument parser creation
 # min-area: minimum size of a moving area to be considered motion. Rules out
@@ -41,9 +29,6 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-a", "--min-area", type = int, default = 500,
                 help = "minimum area size")
 args = vars(ap.parse_args())
-
-#TODO: the tutorial page has a VideoStream object here. May need to add it in
-#TODO: If using the direct VideoCapture doesn't work
 
 # If the capture cannot retrieve an image, report an issue with camera
 if not cap.isOpened():
@@ -62,7 +47,6 @@ while True:
     # Create background mask by subtracting non-moving pixels from frame
     fgmask = fgbg.apply(frame)
     
-    
     # Grab contours from masked image
     contours = cv.findContours(fgmask.copy(), cv.RETR_EXTERNAL,
                                cv.CHAIN_APPROX_SIMPLE)
@@ -76,11 +60,16 @@ while True:
         # Compute the bounding box for the contour and draw it on the frame
         (x, y, w, h) = cv.boundingRect(contour)
         cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Include a timestamp
+        cv.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S:%p"),
+                   (10, frame.shape[0] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.55, (255, 180, 180), 2)
     
     # Display the resulting frame
     cv.imshow('frame', frame)
+    cv.imshow('contour', fgmask)
     cv.startWindowThread()
     
+    # Close when user hits 'esc'
     k = cv.waitKey(30) & 0xff
     if k == 27:
         break
