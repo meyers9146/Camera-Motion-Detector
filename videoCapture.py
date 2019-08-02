0b49809921fc31bf4ece118b990754a1fd1723fb
Mike Meyers <Mike@The-Mikebook-Pro-2.local>
2019-08-02 10:01:44 -0400

    changed videoCapture to have capture.start() and capture.stop() methods

--- videoCapture.py
+++ videoCapture.py
@@ -8,6 +8,11 @@ Create a VideoCapture thread and capture a segment of video from webcam
 @author: Mike Meyers
 """
 
+#TODO: create start() and stop() methods so that there is only a single file
+#TODO: start() trigger will be easy, but how will I know how to stop()?
+#TODO: also, this will rewrite files created within the same minute. Need to make
+#      unique filenames per recording (global var?)
+
 import time
 import datetime
 import numpy as np
@@ -17,40 +22,47 @@ class videoCapture:
     def __init__(self):
         pass
     
-    def capture(self, camera=0, seconds=10):
-        
-        FPS = 20.0 #Frames Per Second: change for higher or lower frame rate
-        
-        # Open VC stream
-        vc = cv2.VideoCapture(camera)
-        
-        # Get video resolution
-        width = int(vc.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(vc.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        print("Resolution is " + str(width) + "x" + str(height)) #TODO: delete later
-        
-        # Create VideoWriter object
-        try:
-            vid_cod = cv2.VideoWriter_fourcc('m','p','4','v')
-            output = cv2.VideoWriter("media/videos/" + datetime.datetime.now().strftime("%Y_%B_%d_%H%M") + ".mov",
-                                     vid_cod, FPS, (width, height), True)
-            print("Video created successfully")
-        except: print("Video file not created successfully")
+    def capture(self, camera=0):
+        count = 1
+        global count
+        running = False
+                
+        def start(self):
+            FPS = 20.0 #Frames Per Second: change for higher or lower frame rate
             
-        # Determine the total number of frames to be recorded
-        frameCount = seconds * FPS
-       
-        # Add each frame to output video
-        currentFrame = 0
-        while currentFrame < frameCount:
-            ret, frame = vc.read()
-            output.write(frame)
-            currentFrame += 1
+            # Open VC stream
+            vc = cv2.VideoCapture(camera)
+            
+            # Get video resolution
+            width = int(vc.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(vc.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            print("Resolution is " + str(width) + "x" + str(height)) #TODO: delete later
+            
+            # Create VideoWriter object
+            try:
+                vid_cod = cv2.VideoWriter_fourcc('m','p','4','v')
+                self.output = cv2.VideoWriter("media/videos/" + datetime.datetime.now().strftime("%Y_%B_%d_%H%M") + ".mov",
+                                         vid_cod, FPS, (width, height), True)
+                print("Video created successfully")
+            except: print("Video file not created successfully")
+                
+           
+            # Add each frame to output video
+            self.currentFrame = 0
+            
+            self.running = True
+        
+        while running == True:
+            ret, frame = self.vc.read()
+            self.output.write(frame)
+            self.currentFrame += 1
+            def stopCapture(self):
+                self.running = False
             
         
         # Close camera and file
-        vc.release()
-        output.release()
+        self.vc.release()
+        self.output.release()
         cv2.destroyAllWindows() #TODO: delete later
     
         # Print on success
